846704edd007767fbc37b0d93c47b0be09855db1
Soggy_Pancake <54160598+Soggy-Pancake@users.noreply.github.com>
2025-09-25 02:35:09 -0700

    Change from fixed signature size to variable signature size. Max encoded size is 255 bytes.

--- src/CodeTalker/Packets/BinaryPacketWrapper.cs
+++ src/CodeTalker/Packets/BinaryPacketWrapper.cs
@@ -3,32 +3,33 @@ using System.Text;
 using CodeTalker.Networking;
 
 internal class BinaryPacketWrapper {
-	internal readonly byte[] Data;
+	internal readonly byte[] FullPacketBytes;
 
 	internal readonly string PacketSignature;
 
     internal BinaryPacketWrapper(string sig, byte[] rawData) {
 
-        string binSig = CodeTalkerNetwork.CODE_TALKER_BINARY_SIGNATURE;
-        int sigSize = CodeTalkerNetwork.BINARY_PACKET_SIG_SIZE;
-
-        if (sig.Length > 8)
-            sig = sig[..sigSize];
-
         PacketSignature = sig;
+        string binSig = CodeTalkerNetwork.CODE_TALKER_BINARY_SIGNATURE;
 
-        int sigLen = binSig.Length + sigSize;
-        binSig += sig;
+        int headerSize = binSig.Length + 1 + signatureBytes.Length;
+
+        // Make header
+        // Header format: CODE_TALKER_BINARY_SIGNATURE PacketSignatureLength(1 Byte) PacketSignature(PacketSigLen bytes long)
+        byte[] header = new byte[headerSize];
+        Array.Copy(Encoding.ASCII.GetBytes(binSig), header, binSig.Length);
+        header[binSig.Length] = (byte)signatureBytes.Length;
+        Array.Copy(signatureBytes, 0, header, binSig.Length + 1, signatureBytes.Length);
 
         // Make a new array with the extra space for the signature and memcopy
-        Data = new byte[sigLen + rawData.Length];
-        Array.Copy(Encoding.ASCII.GetBytes(binSig), Data, binSig.Length);
-        Array.Copy(rawData, 0, Data, sigLen, rawData.Length);
+        FullPacketBytes = new byte[headerSize + rawData.Length];
+        Array.Copy(header, FullPacketBytes, header.Length);
+        Array.Copy(rawData, 0, FullPacketBytes, headerSize, rawData.Length);
     }
 
     internal BinaryPacketWrapper(byte[] rawPacketData) {
-        int sigSize = CodeTalkerNetwork.BINARY_PACKET_SIG_SIZE;
-        PacketSignature = Encoding.ASCII.GetString(rawPacketData, 0, sigSize);
-        Data = rawPacketData[sigSize..];
+        int sigSize = rawPacketData[0];
+        PacketSignature = Encoding.UTF8.GetString(rawPacketData, 1, sigSize);
+        FullPacketBytes = rawPacketData[(sigSize + 1)..];
     }
 }
